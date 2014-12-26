04391c3ab8ca027d16ec08121d8b6d8cc45b36a4
Dan Hardiker <dan@hardiker.co.uk>
2014-12-26 20:10:23 +0000

    Updating so that Java 7 and 8 can be selected independently with a default set

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,18 @@
 #  apt
 # Sample Usage:
 #  include java78
-class java78 {
+class java78 (
+    $versions = [ '7', '8' ],
+    $default = '7',
+  ){
+
+  validate_array($versions)
+
+  $default_ver = $default ? {
+      undef    => $versions[0],
+      $default => $default
+  }
+
   case $::operatingsystem {
     debian: {
       include apt
@@ -19,38 +30,51 @@ class java78 {
         key_server  => 'keyserver.ubuntu.com',
         include_src => true
       }
-      package { 'oracle-java7-installer':
-        responsefile => '/tmp/java7.preseed',
-        require      => [
-                          Apt::Source['webupd8team'],
-                          File['/tmp/java7.preseed']
-                        ],
+
+      if '7' in $versions {
+        package { 'oracle-java7-installer':
+          responsefile => '/tmp/java7.preseed',
+          require      => [
+                            Apt::Source['webupd8team'],
+                            File['/tmp/java7.preseed']
+                          ],
+        }
       }
-      package { 'oracle-java8-installer':
-        responsefile => '/tmp/java8.preseed',
-        require      => [
-                          Apt::Source['webupd8team'],
-                          File['/tmp/java8.preseed']
-                        ],
+
+      if '8' in $versions {
+        package { 'oracle-java8-installer':
+          responsefile => '/tmp/java8.preseed',
+          require      => [
+                            Apt::Source['webupd8team'],
+                            File['/tmp/java8.preseed']
+                          ],
+        }
       }
     }
+
     ubuntu: {
       include apt
 
       apt::ppa { 'ppa:webupd8team/java': }
-      package { 'oracle-java7-installer':
-        responsefile => '/tmp/java7.preseed',
-        require      => [
-                          Apt::Ppa['ppa:webupd8team/java'],
-                          File['/tmp/java7.preseed']
-                        ],
+
+      if '7' in $versions {
+        package { 'oracle-java7-installer':
+          responsefile => '/tmp/java7.preseed',
+          require      => [
+                            Apt::Ppa['ppa:webupd8team/java'],
+                            File['/tmp/java7.preseed']
+                          ],
+        }
       }
-      package { 'oracle-java8-installer':
-        responsefile => '/tmp/java8.preseed',
-        require      => [
-                          Apt::Ppa['ppa:webupd8team/java'],
-                          File['/tmp/java8.preseed']
-                        ],
+
+      if '8' in $versions {
+        package { 'oracle-java8-installer':
+          responsefile => '/tmp/java8.preseed',
+          require      => [
+                            Apt::Ppa['ppa:webupd8team/java'],
+                            File['/tmp/java8.preseed']
+                          ],
+        }
       }
     }
     default: { notice "Unsupported operatingsystem ${::operatingsystem}" }
@@ -58,26 +82,44 @@ class java78 {
 
   case $::operatingsystem {
     debian, ubuntu: {
-      file { '/tmp/java7.preseed':
-        source => 'puppet:///modules/java78/java7.preseed',
-        mode   => '0600',
-        backup => false,
+      if '7' in $versions {
+        file { '/tmp/java7.preseed':
+          source => 'puppet:///modules/java78/java7.preseed',
+          mode   => '0600',
+          backup => false,
+        }
       }
-      file { '/tmp/java8.preseed':
-        source => 'puppet:///modules/java78/java8.preseed',
-        mode   => '0600',
-        backup => false,
+
+      if '8' in $versions {
+        file { '/tmp/java8.preseed':
+          source => 'puppet:///modules/java78/java8.preseed',
+          mode   => '0600',
+          backup => false,
+        }
       }
     }
     default: { notice "Unsupported operatingsystem ${::operatingsystem}" }
   }
 
-  file { '/etc/profile.d/set_java_home.sh':
-    ensure  => file,
-    group   => root,
-    owner   => root,
-    mode    => '0755',
-    source  => 'puppet:///modules/java78/set_java_home.sh',
-    require => Package['oracle-java7-installer']
+  if $default_ver == '7' {
+    file { '/etc/profile.d/set_java_home.sh':
+      ensure  => file,
+      group   => root,
+      owner   => root,
+      mode    => '0755',
+      content => "export JAVA_HOME=/usr/lib/jvm/java-${default_ver}-oracle/\nexport PATH=\${JAVA_HOME}/bin:\${PATH}",
+      require => Package['oracle-java7-installer']
+    }
+  }
+
+  if $default_ver == '8' {
+    file { '/etc/profile.d/set_java_home.sh':
+      ensure  => file,
+      group   => root,
+      owner   => root,
+      mode    => '0755',
+      content => "export JAVA_HOME=/usr/lib/jvm/java-${default_ver}-oracle/\nexport PATH=\${JAVA_HOME}/bin:\${PATH}",
+      require => Package['oracle-java8-installer']
+    }    
   }
 }
